635d165d51d1bed6ff6f81b50ea7c8c5e4ec89f0
chillyfest <111262007+chillyfest@users.noreply.github.com>
2022-08-14 23:56:27 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,36 @@
+// step 1
+
+var myAge = '33';
+
+// step 2
+
+var earlyYears = '2';
+
+// step 3
+
+earlyYears *= 10.5;
+
+// step 4
+
+var laterYears = (myAge - 2);
+
+// step 5
+
+laterYears *= 4;
+
+// step 6
+console.log(`Early years = ${earlyYears}, Later years = ${laterYears}.`);
+
+// step 7
+
+var myAgeInDogYears = earlyYears + laterYears;
+
+// step 8
+
+var myName = ('JAMES'.toLowerCase());
+
+console.log(myName);
+
+// step 9
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
